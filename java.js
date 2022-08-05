11a3d2053b620128f9fc4a949af4259bac184c5d
Kyle <kylevance801@gmail.com>
2022-08-05 15:18:52 -0600

    tested live version, added commets

--- java.js
+++ java.js
@@ -1,15 +1,16 @@
-populateLocalStorage();
 
+populateLocalStorage();
+// Timer that updates in real time
 setInterval(() => { $('#time').text(moment().format('MMMM Do YYYY, H:mm:ss a'));});
 
-
+// Saves the item in local storage on click
 function setKey() {
     var grabID = $(this).parent().attr('id');
     localStorage.setItem(grabID, $('#' + grabID + ' textarea').val());
 };
 
 $('.saveBtn').on('click', setKey);
-  
+//  Assigns the class to each field based on the time of day 
 function assignClass() {
     $('.time-block').each(function() {
         let timeID = parseInt($(this).attr('id'));
@@ -27,7 +28,7 @@ function assignClass() {
     })
 }
 assignClass();
-
+// Populates the time blocks with local storage date (if already entered)
 function populateLocalStorage() {
     $('.time-block').each(function() {
         let getID = $(this).attr('id');

f29c88597d1ed307f86e74c9cfa0ea111bc908be
Kyle <kylevance801@gmail.com>
2022-08-05 14:26:42 -0600

    added the assign class function. I think everything is working as intended. Going to deploy live to make sure it's working on a live deployment vs just on local server

--- java.js
+++ java.js
@@ -1,10 +1,33 @@
 // Timer that updates in real time
-setInterval(() => { $('#time').text(moment().format('MMMM Do YYYY, h:mm:ss a'));});
+
+setInterval(() => { $('#time').text(moment().format('MMMM Do YYYY, H:mm:ss a'));});
 
 function setKey() {
     var grabID = $(this).parent().attr('id');
-    localStorage.setItem(grabID, $('#' + grabID + ' textarea').val());
+    localStorage.setItem(grabID, $(grabID + ' textarea').val());
+    assignClass();
 };
 
 $('.saveBtn').on('click', setKey);
   
+function assignClass() {
+    $('.time-block').each(function() {
+        let timeID = parseInt($(this).attr('id'));
+        let hour = moment().format('H');
+
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).removeClass('future');
+
+        if (timeID > hour) {
+            $(this).addClass('future');
+        } 
+        else if (timeID == hour) {
+            $(this).addClass('present');
+        } 
+        else {
+            $(this).addClass('past');
+        }
+    })
+}
+assignClass();
